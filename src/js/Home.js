2df27fd5be2a8b106e81a26dffe98dfeb33e387a
Arthur Le Meur <arthur@edrlab.org>
2023-02-01 10:08:55 +0100

    Commit 1 - Initialisation React+React Router

--- src/js/Home.js
+++ src/js/Home.js
@@ -1,13 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from '../assets/logo.svg';
+import '../styles//Home.css';
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 
-function App() {
+function Home() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/Home.js</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -22,4 +23,4 @@ function App() {
   );
 }
 
-export default App;
+export default Home;
